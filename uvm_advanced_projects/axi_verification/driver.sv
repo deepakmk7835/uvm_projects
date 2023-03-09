757a69adfe18816253f682d8e3e76d2bbc32afd8
deepakmk7835 <deepakmk7835@gmail.com>
2023-03-09 19:14:55 +0000

    added Incr write read, wrap write read and error testcases in driver class

--- uvm_advanced_projects/axi_verification/driver.sv
+++ uvm_advanced_projects/axi_verification/driver.sv
@@ -141,19 +141,262 @@ class driver extends uvm_driver#(transaction);
 
 		vif.araddr <= 5; //32-vif.address
 		vif.arvalid<= 1; //Indicates valid address on WAC
-		vif.arlen<=   7; //awlen+1 = transactions
+		vif.arlen<=   7; //arlen+1 = transactions
 		vif.arburst<= 0; //Type of burst = 0 fixed, 1 incr, 2 wrap
-		vif.arsize<=  2; // Max number of bytes allowed per transaction for 4 bytes awsize = 2 i.e bytes = 2 ^ awsize
+		vif.arsize<=  2; // Max number of bytes allowed per transaction for 4 bytes arsize = 2 i.e bytes = 2 ^ arsize
 		vif.arid <= t.arid;
 		
-		vif.rready <= 1;
+		vif.rready <= 1; //Ready to receive data from slave
 	
-		for(int i=0;i<(vif.arlen+1);i++)begin
-			@(posedge vif.arready);
+		for(int i=0;i<(vif.arlen+1);i++)begin //Receive 8 TXN each 4 bytes wide
+			@(posedge vif.arready); //Indicates completion of single read TXN
 			@(posedge vif.clk);
 		end
 
-		@(negedge vif.rlast); //Indicates completion of write transaction
+		@(negedge vif.rlast); //Indicates last read transaction
+		vif.rready <= 0;	
+		vif.arvalid <= 0;	
+		
+	endtask
+
+	//Increment Write TXN
+	
+	task incrBurstWr();
+		//WAC
+		vif.awaddr <= 5; //32-vif.address
+		vif.awvalid<= 1; //Indicates valid address on WAC
+		vif.awlen<=   7; //awlen+1 = transactions
+		vif.awburst<= 1; //Type of burst = 0 fixed, 1 incr, 2 wrap
+		vif.awsize<=  2; // Max number of bytes allowed per transaction for 4 bytes awsize = 2 i.e bytes = 2 ^ awsize
+		vif.awid <= t.awid;
+
+		//Write Data Channel (WDC)
+		
+		vif.wdata<= t.wdata; //32-vif.data 
+		vif.wstrb<= t.wstrb; //Indicates which data lane has valid data
+		vif.wvalid<= 1; //Indicates valid data on WDC
+		vif.wid <= t.wid;
+		vif.wlast <= 0;
+
+		vif.arvalid <= 0;
+		vif.rready <= 0;
+		
+		@(posedge vif.wready); //Indicates completion of write transaction
+	
+		for(int i = 0 ;i<vif.awlen; i++)begin
+			@(posedge vif.clk); //Drive new data active edge of the clock
+			vif.wdata <= $urandom_range(0,10);
+			@(posedge vif.wready); //Indicates completion of write transaction
+		end
+
+		vif.awvalid <= 0;
+		vif.wvalid <= 0;
+		vif.wlast <= 1;
+
+		//Write Response Channel (WRC)
+		
+		vif.bready<= 1;
+		
+		@(negedge bvalid);
+		vif.bready <= 0;
+	endtask
+
+	task incrBurstRd();
+		//WAC
+		vif.awaddr <= 0; //32-vif.address
+		vif.awvalid<= 0; //Indicates valid address on WAC
+		vif.awlen<=   0; //awlen+1 = transactions
+		vif.awburst<= 0; //Type of burst = 0 fixed, 1 incr, 2 wrap
+		vif.awsize<=  0; // Max number of bytes allowed per transaction for 4 bytes awsize = 2 i.e bytes = 2 ^ awsize
+		vif.awid <= 0;
+
+		//Write Data Channel (WDC)
+		
+		vif.wdata<= 0; //32-vif.data 
+		vif.wstrb<= 0; //Indicates which data lane has valid data
+		vif.wvalid<= 0; //Indicates valid data on WDC
+		vif.wid <= 0;
+		vif.wlast <= 0;
+		
+		//RAC
+
+		vif.araddr <= 5; //32-vif.address
+		vif.arvalid<= 1; //Indicates valid address on WAC
+		vif.arlen<=   7; //arlen+1 = transactions
+		vif.arburst<= 1; //Type of burst = 0 fixed, 1 incr, 2 wrap
+		vif.arsize<=  2; // Max number of bytes allowed per transaction for 4 bytes arsize = 2 i.e bytes = 2 ^ arsize
+		vif.arid <= t.arid;
+		
+		vif.rready <= 1; //Ready to receive data from slave
+	
+		for(int i=0;i<(vif.arlen+1);i++)begin //Receive 8 TXN each 4 bytes wide
+			@(posedge vif.arready); //Indicates completion of single read TXN
+			@(posedge vif.clk);
+		end
+
+		@(negedge vif.rlast); //Indicates last read transaction
+		vif.rready <= 0;	
+		vif.arvalid <= 0;	
+		
+	endtask
+
+	//Wrap Write Read TXN
+	
+	task wrapBurstWr();
+		//WAC
+		vif.awaddr <= 5; //32-vif.address
+		vif.awvalid<= 1; //Indicates valid address on WAC
+		vif.awlen<=   7; //awlen+1 = transactions
+		vif.awburst<= 2; //Type of burst = 0 fixed, 1 incr, 2 wrap
+		vif.awsize<=  2; // Max number of bytes allowed per transaction for 4 bytes awsize = 2 i.e bytes = 2 ^ awsize
+		vif.awid <= t.awid;
+
+		//Write Data Channel (WDC)
+		
+		vif.wdata<= t.wdata; //32-vif.data 
+		vif.wstrb<= t.wstrb; //Indicates which data lane has valid data
+		vif.wvalid<= 1; //Indicates valid data on WDC
+		vif.wid <= t.wid;
+		vif.wlast <= 0;
+
+		vif.arvalid <= 0;
+		vif.rready <= 0;
+		
+		@(posedge vif.wready); //Indicates completion of write transaction
+	
+		for(int i = 0 ;i<vif.awlen; i++)begin
+			@(posedge vif.clk); //Drive new data active edge of the clock
+			vif.wdata <= $urandom_range(0,10);
+			@(posedge vif.wready); //Indicates completion of write transaction
+		end
+
+		vif.awvalid <= 0;
+		vif.wvalid <= 0;
+		vif.wlast <= 1;
+
+		//Write Response Channel (WRC)
+		
+		vif.bready<= 1;
+		
+		@(negedge bvalid);
+		vif.bready <= 0;
+	endtask
+
+	task wrapBurstRd();
+		//WAC
+		vif.awaddr <= 0; //32-vif.address
+		vif.awvalid<= 0; //Indicates valid address on WAC
+		vif.awlen<=   0; //awlen+1 = transactions
+		vif.awburst<= 0; //Type of burst = 0 fixed, 1 incr, 2 wrap
+		vif.awsize<=  0; // Max number of bytes allowed per transaction for 4 bytes awsize = 2 i.e bytes = 2 ^ awsize
+		vif.awid <= 0;
+
+		//Write Data Channel (WDC)
+		
+		vif.wdata<= 0; //32-vif.data 
+		vif.wstrb<= 0; //Indicates which data lane has valid data
+		vif.wvalid<= 0; //Indicates valid data on WDC
+		vif.wid <= 0;
+		vif.wlast <= 0;
+		
+		//RAC
+
+		vif.araddr <= 5; //32-vif.address
+		vif.arvalid<= 1; //Indicates valid address on WAC
+		vif.arlen<=   7; //arlen+1 = transactions
+		vif.arburst<= 2; //Type of burst = 0 fixed, 1 incr, 2 wrap
+		vif.arsize<=  2; // Max number of bytes allowed per transaction for 4 bytes arsize = 2 i.e bytes = 2 ^ arsize
+		vif.arid <= t.arid;
+		
+		vif.rready <= 1; //Ready to receive data from slave
+	
+		for(int i=0;i<(vif.arlen+1);i++)begin //Receive 8 TXN each 4 bytes wide
+			@(posedge vif.arready); //Indicates completion of single read TXN
+			@(posedge vif.clk);
+		end
+
+		@(negedge vif.rlast); //Indicates last read transaction
+		vif.rready <= 0;	
+		vif.arvalid <= 0;	
+		
+	endtask
+
+	//Error Write
+	
+	task ErrWr();
+		//WAC
+		vif.awaddr <= 128; //32-vif.address Since address is 128, this is out of range according to the design
+		vif.awvalid<= 1; //Indicates valid address on WAC
+		vif.awlen<=   7; //awlen+1 = transactions
+		vif.awburst<= 0; //Type of burst = 0 fixed, 1 incr, 2 wrap
+		vif.awsize<=  2; // Max number of bytes allowed per transaction for 4 bytes awsize = 2 i.e bytes = 2 ^ awsize
+		vif.awid <= t.awid;
+
+		//Write Data Channel (WDC)
+		
+		vif.wdata<= t.wdata; //32-vif.data 
+		vif.wstrb<= t.wstrb; //Indicates which data lane has valid data
+		vif.wvalid<= 1; //Indicates valid data on WDC
+		vif.wid <= t.wid;
+		vif.wlast <= 0;
+
+		vif.arvalid <= 0;
+		vif.rready <= 0;
+		
+		@(posedge vif.wready); //Indicates completion of write transaction
+	
+		for(int i = 0 ;i<vif.awlen; i++)begin
+			@(posedge vif.clk); //Drive new data active edge of the clock
+			vif.wdata <= $urandom_range(0,10);
+			@(posedge vif.wready); //Indicates completion of write transaction
+		end
+
+		vif.awvalid <= 0;
+		vif.wvalid <= 0;
+		vif.wlast <= 1;
+
+		//Write Response Channel (WRC)
+		
+		vif.bready<= 1;
+		
+		@(negedge bvalid);
+		vif.bready <= 0;
+	endtask
+
+	task ErrRd();
+		//WAC
+		vif.awaddr <= 0; //32-vif.address
+		vif.awvalid<= 0; //Indicates valid address on WAC
+		vif.awlen<=   0; //awlen+1 = transactions
+		vif.awburst<= 0; //Type of burst = 0 fixed, 1 incr, 2 wrap
+		vif.awsize<=  0; // Max number of bytes allowed per transaction for 4 bytes awsize = 2 i.e bytes = 2 ^ awsize
+		vif.awid <= 0;
+
+		//Write Data Channel (WDC)
+		
+		vif.wdata<= 0; //32-vif.data 
+		vif.wstrb<= 0; //Indicates which data lane has valid data
+		vif.wvalid<= 0; //Indicates valid data on WDC
+		vif.wid <= 0;
+		vif.wlast <= 0;
+		
+		//RAC
+
+		vif.araddr <= 128; //32-vif.address
+		vif.arvalid<= 1; //Indicates valid address on WAC
+		vif.arlen<=   7; //arlen+1 = transactions
+		vif.arburst<= 0; //Type of burst = 0 fixed, 1 incr, 2 wrap
+		vif.arsize<=  2; // Max number of bytes allowed per transaction for 4 bytes arsize = 2 i.e bytes = 2 ^ arsize
+		vif.arid <= t.arid;
+		
+		vif.rready <= 1; //Ready to receive data from slave
+	
+		for(int i=0;i<(vif.arlen+1);i++)begin //Receive 8 TXN each 4 bytes wide
+			@(posedge vif.arready); //Indicates completion of single read TXN
+			@(posedge vif.clk);
+		end
+
+		@(negedge vif.rlast); //Indicates last read transaction
 		vif.rready <= 0;	
 		vif.arvalid <= 0;	
 		
@@ -169,9 +412,28 @@ class driver extends uvm_driver#(transaction);
 	
 	//Task for Incremental Write Read Burst
 	
+	task wrRdIncrBurst();
+		vif.rstn <= 1'b1;
+		incrBurstWr();	
+		incrBurstRd();
+	endtask
+
 	//Task for Wrap Write Read Burst
 	
+	task wrRdWrapBurst();
+		vif.rstn <= 1'b1;
+		wrapBurstWr();	
+		wrapBurstRd();
+	endtask
+
+
 	//Task for Error Test Case
+	
+	task wrRdErr();
+		vif.rstn <= 1'b1;
+		ErrWr();	
+		ErrRd();
+	endtask
 
 	virtual task run_phase();
 
